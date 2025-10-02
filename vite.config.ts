f8beea5789176c6e469967e5e5d1d32ebe0a5f9b
Testname <nataliadel7@gmail.com>
2025-10-02 17:21:15 +0200

    add Workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "my-project",
   plugins: [tailwindcss()],
   content: ["./index.html", "./impressum.html"],
   theme: {
